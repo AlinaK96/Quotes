607f3ef5bc48827ce00ca88cc2ad6bd26d0d1d20
Alina <AliKochneva@yandex.ru>
2023-08-23 12:06:38 +0700

    загрулшка на странице с избранными цитататми

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,13 +1,32 @@
 import React from "react"
 import CustomBtn from "../components/UI/customLink";
+import FavQuotes from "../components/FavQuotesList";
 
 const Favourites = () => {
-return (
-    <div>
-        <CustomBtn title='All quotes' link='/' />
-        <p className="favourites__header">My favourite quotes: </p>
-    </div>
-)
+    const Favquotes = [{
+        id: 1,
+        quote:"I'm not a great writer. I write badly.",
+        author:'Ne',
+    },
+    {
+        id: 2,
+        quote:"I'm not a great writer. I write badly.",
+        author:'Ne',
+    },
+    {
+        id: 3,
+        quote:"I'm not a great writer. I write badly.",
+        author:'Ne',
+    }
+    
+    ]
+    return (
+        <div>
+            <CustomBtn title='All quotes' link='/' />
+            <p className="favourites__header">My favourite quotes: </p>
+            <FavQuotes Favquotes={Favquotes}/>
+        </div>
+    )
 };
 
 export default Favourites;
