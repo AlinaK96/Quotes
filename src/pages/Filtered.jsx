c6412608db27f388e220f26acb1743ceda637706
Alina <AliKochneva@yandex.ru>
2023-08-30 18:44:48 +0700

    сортировка по алфавиту

--- src/pages/Filtered.jsx
+++ src/pages/Filtered.jsx
@@ -8,6 +8,12 @@ const FilteredList = ({quotes, setFavorites, favorites}) => {
     const filteredquotes = useMemo(() => [[author, i => i.author === author],]
         .reduce((acc, i) => i[0] ? acc.filter(i[1]) : acc, quotes),[ quotes, author ]);
 
+    filteredquotes.sort(function(x, y) {
+        if(x.author.toLowerCase() < y.author.toLowerCase()) return -1;
+        if(x.author.toLowerCase() > y.author.toLowerCase()) return 1;
+        return 0;
+    })
+
     const addToFavourites = (i) => {
         if(!favorites.includes(i)){
             setFavorites([...favorites, i]);
